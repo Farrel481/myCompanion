fd0e41a4fb5db1527530f259c5e4312139102d9e
Farrel481 <Farrelsatriano2@gmail.com>
2026-07-21 05:01:31 +0700

    total revision, model and acts

--- companion_widget.py
+++ companion_widget.py
@@ -1,132 +1,133 @@
 import random
-from PySide6.QtCore import QTimer, Qt
-from PySide6.QtWidgets import QLabel, QWidget
+
 from pathlib import Path
+from PySide6.QtCore import QTimer, Qt
 from PySide6.QtGui import QPixmap
+from PySide6.QtWidgets import QLabel, QWidget
 
 class Companion(QWidget):
+    BLOB_X = 10
+    BLOB_Y = 0
+
     def __init__(self):
         super().__init__()
         self.drag_offset = None
         self.is_carried = False
-        self.bob_step = 0
+        self.bob_step = 0 
+        self.default_position_set = False
 
-        self.activity = "walking"
-        self.behavior_timer = QTimer(self)
-        self.behavior_timer.setSingleShot(True)
-        self.behavior_timer.timeout.connect(self.change_activity)
-        self.schedule_next_activity()
-
-        self.walk_speed = 3
-        self.walk_direction = 1
-        self.walk_timer = QTimer(self)
-        self.walk_timer.setInterval(30)
-        self.walk_timer.timeout.connect(self.walk_one_step)
-        self.walk_timer.start()
-
-        self.setWindowFlags(
-            Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint 
-        )
+        self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
         self.setAttribute(Qt.WA_TranslucentBackground)
         self.setFixedSize(180, 180)
         self.setCursor(Qt.OpenHandCursor)
-        
-        sprite_path = (Path(__file__).parent/"assets"/"sprites"/"cat_idle_1.png")
-        sprite = QPixmap(str(sprite_path)) 
-        if sprite.isNull():
-            raise FileNotFoundError(f"Sprite file not found, error has occured {sprite_path}.")
-        self.blob = QLabel(parent = self)
-        scaled_sprite = sprite.scaled(160, 160, Qt.KeepAspectRatio, Qt.FastTransformation)
 
-        self.blob.setPixmap(scaled_sprite)
+        sprite_folder = Path(__file__).parent/"assets"/"sprites"
+        idle_1 = QPixmap(str(sprite_folder/"slime_idle_1.png"))
+        idle_2 = QPixmap(str(sprite_folder/"slime_idle_2.png"))
+        
+        if idle_1.isNull() or idle_2.isNull():
+            raise FileNotFoundError("File on assets folder can't be read. Check.")
+        
+        self.idle_frames = [
+            idle_1.scaled(160, 160, Qt.KeepAspectRatio, Qt.FastTransformation),
+            idle_2.scaled(160, 160, Qt.KeepAspectRatio, Qt.FastTransformation),
+        ]
+        self.idle_frame_index = 0
+
+        self.blob = QLabel(parent = self)
+        self.blob.setPixmap(self.idle_frames[self.idle_frame_index])
         self.blob.setFixedSize(160, 160)
         self.blob.setAttribute(Qt.WA_TransparentForMouseEvents)
-        self.blob.move(10, 0)
+        self.blob.move(self.BLOB_X, self.BLOB_Y)
 
-        self.hint = QLabel("Drag me!", parent=self)
+        self.hint = QLabel(parent=self)
         self.hint.setStyleSheet("""color: white; font-size: 12px;""")
         self.hint.setAttribute(Qt.WA_TransparentForMouseEvents)
-        self.center_hint()
-        
-        self.animation_timer = QTimer(self)
-        self.animation_timer.setInterval(100)
-        self.animation_timer.timeout.connect(self.animate_carried)
+        self.hint.hide()
 
-        self.idle_step = 0
         self.idle_timer = QTimer(self)
-        self.idle_timer.setInterval(400)
+        self.idle_timer.setSingleShot(True)
         self.idle_timer.timeout.connect(self.animate_idle)
 
-    def walk_one_step(self):
-        if self.is_carried or self.activity != "walking":
-            return
-        
+        self.carry_timer = QTimer(self)
+        self.carry_timer.setInterval(100)
+        self.carry_timer.timeout.connect(self.animate_carried)
+
+    def default_place(self):
         current_screen = self.screen()
         if current_screen is None:
             return
-        
         screen_area = current_screen.availableGeometry()
-        next_x = self.x() + self.walk_speed * self.walk_direction
-        left_limit = screen_area.left()
-        right_limit = screen_area.right() - self.width()
 
-        if next_x <= left_limit or next_x >= right_limit:
-            self.walk_direction *= -1
-            next_x = self.x() + self.walk_speed * self.walk_direction
+        x_position = (screen_area.right() - self.width() - 15)
+        y_position = (screen_area.bottom() - self.height() + 1)
 
-        self.move(next_x, self.y())
+        self.move(x_position, y_position)
 
-    def land_on_ground(self):
-        current_screen = self.screen()
-        if current_screen is None:
+    def schedule_blink(self):
+        if self.is_carried:
             return
+        if self.idle_frame_index == 0:
+            delay=random.randint(3200, 11200)
+        else:
+            delay= 180
         
-        screen_area = current_screen.availableGeometry()
-        ground_y = screen_area.bottom() - self.height() + 1
-        self.move(self.x(), ground_y)
+        self.idle_timer.start(delay)
+    
+    def animate_idle(self):
+        if self.is_carried:
+            return
+        self.idle_frame_index = (self.idle_frame_index+ 1) %len (self.idle_frames)
 
-    def showEvent(self, event):
-        super().showEvent(event)
-        self.land_on_ground()
+        self.blob.setPixmap(self.idle_frames[self.idle_frame_index])
+
+        self.schedule_blink()
 
     def center_hint(self):
         self.hint.adjustSize()
-        x_position = (self.width() - self.hint.width()) // 2
+
+        x_position =(self.width() - self.hint.width()) // 2
+
         self.hint.move(x_position, 145)
 
     def set_carried(self, carried):
         self.is_carried = carried
-        if carried:
-            self.hint.setText("Hey whachu doin!")
-            self.animation_timer.start()
-            self.center_hint()
-        else:
-            self.animation_timer.stop()
-            self.apply_activity_visual()
 
-        
+        if carried:
+            self.idle_timer.stop()
+            self.hint.setText("Hey! Whachu doin?")
+            self.center_hint()
+            self.hint.show()
+            self.carry_timer.start()
+
+        else:
+            self.carry_timer.stop()
+            self.blob.move(self.BLOB_X, self.BLOB_Y)
+            self.hint.hide()
+            self.schedule_blink()
 
     def animate_carried(self):
         if not self.is_carried:
             return
-        self.bob_step = (self.bob_step + 1) % 2
+            
+        self.bob_step = (self.bob_step + 1)%2
+
         if self.bob_step == 0:
-            self.blob.move(24, 3)
+            self.blob.move(self.BLOB_X, 0)
         else:
-            self.blob.move(24, 8)
+            self.blob.move(self.BLOB_X, 7)
 
     def mousePressEvent(self, event):
         if event.button() == Qt.LeftButton:
-            self.drag_offset = (
-                event.globalPosition().toPoint() - self.frameGeometry().topLeft()
-            )
+            self.drag_offset = (event.globalPosition().toPoint() - self.frameGeometry().topLeft())
 
             self.setCursor(Qt.ClosedHandCursor)
             self.set_carried(True)
-
+    
     def mouseMoveEvent(self, event):
-        if event.buttons() & Qt.LeftButton and self.drag_offset is not None:
-            new_position = event.globalPosition().toPoint() - self.drag_offset
+        if (event.buttons() & Qt.LeftButton and self.drag_offset is not None):
+            new_position = (event.globalPosition().toPoint() - self.drag_offset)
+
             self.move(new_position)
 
     def mouseReleaseEvent(self, event):
@@ -135,50 +136,12 @@ class Companion(QWidget):
             self.setCursor(Qt.OpenHandCursor)
             self.set_carried(False)
 
-        self.land_on_ground()
+    def showEvent(self, event):
+        super().showEvent(event)
+        if not self.default_position_set:
+            self.default_place()
+            self.default_position_set = True
 
-    def schedule_next_activity(self):
-        if self.activity == "walking":
-            delay = random.randint(3500, 6500)
-        else:
-            delay = random.randint(1500, 3000)
-        self.behavior_timer.start(delay)
+        self.schedule_blink()
+        
 
-    def change_activity(self):
-        if self.is_carried:
-            self.schedule_next_activity()
-            return
-        if self.activity == "walking":
-            self.activity = "idle"
-        else:
-            self.activity = "walking"
-
-        self.apply_activity_visual()
-        self.schedule_next_activity()
-    
-    def apply_activity_visual(self):
-        if self.activity == "idle":
-            self.blob.setStyleSheet("""color: #5B3E9D; font-size: 120px;""")
-            self.blob.move(24, 18)
-            self.hint.setText("...")
-            self.idle_timer.start()
-
-        else:
-            self.blob.setStyleSheet("""color: #8B5CF6; font-size: 130px;""")
-            self.blob.move(24, 8)
-            self.hint.setText("Walking...")
-            self.idle_timer.stop()
-
-        self.center_hint()
-
-    def animate_idle(self):
-        if self.is_carried or self.activity != "idle":
-            return
-        self.idle_step = (self.idle_step + 1) % 2
-
-        if self.idle_step == 0:
-            self.blob.setStyleSheet("""color: #5B3E9D ;font-size: 120px;""")
-            self.blob.move(24, 18)
-        else:
-            self.blob.setStyleSheet("""color: #6B49B5 ;font-size: 116px;""")
-            self.blob.move(27, 22)
